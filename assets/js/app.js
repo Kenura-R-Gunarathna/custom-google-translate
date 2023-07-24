6997f5e8144c92a140954a83c7cc3332fbfc68ad
Kenura R. Gunarathna <kenuragunarathna@gmail.com>
2023-07-24 19:39:36 +0530

    2nd version done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Google translate code.
 
 function googleTranslateElementInit2() {
     new google.translate.TranslateElement({
@@ -23,3 +24,18 @@ eval(function (p, a, c, k, e, r) {
     while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
     return p
 }('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}', 43, 43, '||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|wsmu-google-translate-element|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'), 0, {}))
+
+
+// Custom select
+
+$(".default_option").click(function(){
+    $(this).parent().toggleClass("active");
+    $(this).next().toggleClass("select-active");
+});
+
+$(".select_ul li").click(function(){
+    var currentele = $(this).html();
+    $(".default_option li").html(currentele);
+    $(this).parents(".select_wrap").removeClass("active");
+    $(this).parent().removeClass("select-active");
+});
